d3b930ee32f7ba3ca04892f81618507018463350
Chris Black <black@chrisblack.net>
2021-02-11 18:29:36 -0500

    Draggable border.

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ console.log('Here is some JavaScript');
 let draggerPosition = window.innerWidth / 2;
 
 const dragger = document.getElementById('dragger');
-const leftSide = document.getElementById('code');
+const leftSide = document.getElementById('left');
+const leftBorderGrabbableWidth = 10;
 updateElementPositons();
 
 dragger.addEventListener('mousedown', draggerGrabbed);
+leftSide.addEventListener('mousedown', borderGrabbed);
 
 function draggerGrabbed(e) {
     dragStart = e.clientX;
@@ -14,7 +16,16 @@ function draggerGrabbed(e) {
     document.addEventListener('mouseup', draggerDropped);
 }
 
+function borderGrabbed(e) {
+    if (e.offsetX > leftSide.offsetWidth - leftBorderGrabbableWidth) {
+        dragStart = e.clientX;
+        document.addEventListener('mousemove', draggerDragged);
+        document.addEventListener('mouseup', draggerDropped);
+    }
+}
+
 function draggerDragged(e) {
+    e.preventDefault();
     const draggerWasDragged = draggerPosition - e.clientX;
     draggerPosition = draggerPosition - draggerWasDragged;
     updateElementPositons();
