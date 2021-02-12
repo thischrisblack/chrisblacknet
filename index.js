a42e3dc72a12eb8c3a9ee174e37aa2491dc28c6a
Chris Black <black@chrisblack.net>
2021-02-12 18:24:44 -0500

    Implement touch events.

--- index.js
+++ index.js
@@ -4,36 +4,48 @@ let draggerPosition = window.innerWidth / 2;
 
 const dragger = document.getElementById('dragger');
 const leftSide = document.getElementById('left');
-const leftBorderGrabbableWidth = 10;
+const leftBorderGrabbableWidth = 20;
 updateElementPositons();
 
 dragger.addEventListener('mousedown', draggerGrabbed);
+dragger.addEventListener('touchstart', draggerGrabbed);
 leftSide.addEventListener('mousedown', borderGrabbed);
+leftSide.addEventListener('touchstart', borderGrabbed);
 
 function draggerGrabbed(e) {
-    dragStart = e.clientX;
+    dragStart = e.clientX || e.changedTouches[0].clientX;
     document.addEventListener('mousemove', draggerDragged);
+    document.addEventListener('touchmove', draggerDragged);
     document.addEventListener('mouseup', draggerDropped);
+    document.addEventListener('touchend', draggerDropped);
 }
 
 function borderGrabbed(e) {
-    if (e.offsetX > leftSide.offsetWidth - leftBorderGrabbableWidth) {
-        dragStart = e.clientX;
+    const start = e.clientX || e.changedTouches[0].clientX;
+    if (start > leftSide.offsetWidth - leftBorderGrabbableWidth) {
+        dragStart = start;
         document.addEventListener('mousemove', draggerDragged);
+        document.addEventListener('touchmove', draggerDragged);
         document.addEventListener('mouseup', draggerDropped);
+        document.addEventListener('touchend', draggerDropped);
     }
 }
 
 function draggerDragged(e) {
-    e.preventDefault();
-    const draggerWasDragged = draggerPosition - e.clientX;
+    if (e.clientX) {
+        e.preventDefault();
+    }
+    const draggerWasDragged =
+        draggerPosition - (e.clientX || e.changedTouches[0].clientX);
     draggerPosition = draggerPosition - draggerWasDragged;
     updateElementPositons();
 }
 
 function draggerDropped() {
     document.removeEventListener('mousemove', draggerDragged);
+    document.removeEventListener('touchmove', draggerDragged);
     document.removeEventListener('mouseup', draggerDropped);
+    document.removeEventListener('touchend', draggerDropped);
 }
 
 function updateElementPositons() {
