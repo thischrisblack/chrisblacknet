80950d073733614f9f3340950c0e45d496d8b3a4
Chris Black <black@chrisblack.net>
2021-02-14 17:25:53 -0500

    Added train picture. Made dragger an IIFE.

--- js/dragger.js
+++ js/dragger.js
@@ -1,57 +1,59 @@
-let draggerPosition = window.innerWidth * 0.42;
+(function dragger() {
+    let draggerPosition = window.innerWidth * 0.475;
 
-const dragger = document.getElementById('dragger');
-const leftSide = document.getElementById('left');
-const leftBorderGrabbableWidth = 20;
-updateElementPositons();
+    const dragger = document.getElementById('dragger');
+    const leftSide = document.getElementById('left');
+    const leftBorderGrabbableWidth = 20;
+    updateElementPositons();
 
-dragger.addEventListener('mousedown', draggerGrabbed);
-dragger.addEventListener('touchstart', draggerGrabbed);
-leftSide.addEventListener('mousedown', borderGrabbed);
-leftSide.addEventListener('touchstart', borderGrabbed);
+    dragger.addEventListener('mousedown', draggerGrabbed);
+    dragger.addEventListener('touchstart', draggerGrabbed);
+    leftSide.addEventListener('mousedown', borderGrabbed);
+    leftSide.addEventListener('touchstart', borderGrabbed);
 
-function draggerGrabbed(e) {
-    dragStart = e.clientX || e.changedTouches[0].clientX;
-    document.addEventListener('mousemove', draggerDragged);
-    document.addEventListener('touchmove', draggerDragged);
-    document.addEventListener('mouseup', draggerDropped);
-    document.addEventListener('touchend', draggerDropped);
-}
-
-function borderGrabbed(e) {
-    const start = e.clientX || e.changedTouches[0].clientX;
-    if (start > leftSide.offsetWidth - leftBorderGrabbableWidth) {
-        dragStart = start;
+    function draggerGrabbed(e) {
+        dragStart = e.clientX || e.changedTouches[0].clientX;
         document.addEventListener('mousemove', draggerDragged);
         document.addEventListener('touchmove', draggerDragged);
         document.addEventListener('mouseup', draggerDropped);
         document.addEventListener('touchend', draggerDropped);
     }
-}
 
-function draggerDragged(e) {
-    if (e.clientX) {
-        e.preventDefault();
+    function borderGrabbed(e) {
+        const start = e.clientX || e.changedTouches[0].clientX;
+        if (start > leftSide.offsetWidth - leftBorderGrabbableWidth) {
+            dragStart = start;
+            document.addEventListener('mousemove', draggerDragged);
+            document.addEventListener('touchmove', draggerDragged);
+            document.addEventListener('mouseup', draggerDropped);
+            document.addEventListener('touchend', draggerDropped);
+        }
     }
-    const draggerWasDragged =
-        draggerPosition - (e.clientX || e.changedTouches[0].clientX);
-    if (
-        draggerPosition - draggerWasDragged > 0 ||
-        draggerPosition - draggerWasDragged < window.innerWidth
-    ) {
-        draggerPosition = draggerPosition - draggerWasDragged;
+
+    function draggerDragged(e) {
+        if (e.clientX) {
+            e.preventDefault();
+        }
+        const draggerWasDragged =
+            draggerPosition - (e.clientX || e.changedTouches[0].clientX);
+        if (
+            draggerPosition - draggerWasDragged > 0 ||
+            draggerPosition - draggerWasDragged < window.innerWidth
+        ) {
+            draggerPosition = draggerPosition - draggerWasDragged;
+        }
+        updateElementPositons();
     }
-    updateElementPositons();
-}
 
-function draggerDropped() {
-    document.removeEventListener('mousemove', draggerDragged);
-    document.removeEventListener('touchmove', draggerDragged);
-    document.removeEventListener('mouseup', draggerDropped);
-    document.removeEventListener('touchend', draggerDropped);
-}
+    function draggerDropped() {
+        document.removeEventListener('mousemove', draggerDragged);
+        document.removeEventListener('touchmove', draggerDragged);
+        document.removeEventListener('mouseup', draggerDropped);
+        document.removeEventListener('touchend', draggerDropped);
+    }
 
-function updateElementPositons() {
-    dragger.style.left = `${draggerPosition}px`;
-    leftSide.style.width = `${draggerPosition}px`;
-}
+    function updateElementPositons() {
+        dragger.style.left = `${draggerPosition}px`;
+        leftSide.style.width = `${draggerPosition}px`;
+    }
+})();
