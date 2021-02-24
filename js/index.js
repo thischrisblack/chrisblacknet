ff0caba8405f54d515a8baf9ae457cb1f24e1a5b
Chris Black <black@chrisblack.net>
2021-02-24 07:36:43 -0500

    Fixed height bug. Added semantic tags. Added more code copy.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
 console.log('Here is some JavaScript');
+
+document.addEventListener('DOMContentLoaded', setHeights);
+
+function setHeights() {
+    const height = document.body.scrollHeight;
+    console.log(height);
+    document.getElementById('left').style.height = `${height}px`;
+    document.getElementById('right').style.height = `${height}px`;
+}
